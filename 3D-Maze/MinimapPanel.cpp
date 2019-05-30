6e3e65c9aa3b9161a439e16b3a1ae36cd4c8b987
Vectrom <damian.plociennik@mybboard.pl>
2019-05-30 19:27:44 +0200

    Added unworking minimap

--- 3D-Maze/MinimapPanel.cpp
+++ 3D-Maze/MinimapPanel.cpp
@@ -1,4 +1,38 @@
+#include <wx/dcbuffer.h>
 #include "MinimapPanel.h"
+#include "Settings.h"
 
 MinimapPanel::MinimapPanel(wxWindow* parent) :
-	BaseMinimapPanel(parent) {}
+	BaseMinimapPanel(parent) {
+	_floorImg.LoadFile(wxT("Textures/Floor.png"), wxBITMAP_TYPE_ANY);
+
+	int sizeY = Settings::worldMap.size();
+	int sizeX = Settings::worldMap[0].size();
+	_boxSize = wxSize(GetSize().x / sizeX, GetSize().y / sizeY);
+	_translation = wxPoint((GetSize().x - _boxSize.x * sizeX) / 2, (GetSize().y - _boxSize.y * sizeY) / 2);
+
+	for (int x = 0; x < sizeX; x++) {
+		std::vector<FieldBox> row;
+		for (int y = 0; y < sizeY; y++)
+			row.push_back(FieldBox(_floorImg, wxPoint(x * _boxSize.x, y * _boxSize.y)));
+
+		_fields.push_back(row);
+	}
+	Layout();
+	draw();
+}
+
+void MinimapPanel::draw() {
+	int sizeY = Settings::worldMap.size();
+	int sizeX = Settings::worldMap[0].size();
+
+	wxClientDC DC(_minimapPanel);
+	wxBufferedDC buffDC(&DC);
+	buffDC.Clear();
+
+	for (int x = 0; x < sizeX; x++) {
+		for (int y = 0; y < sizeY; y++) {
+			buffDC.DrawBitmap(_fields[x][y]._img.Scale(_boxSize.x, _boxSize.y), _fields[x][y]._position + _translation);
+		}
+	}
+}
--- 3D-Maze/MinimapPanel.h
+++ 3D-Maze/MinimapPanel.h
@@ -1,9 +1,22 @@
 #pragma once
-
 #include "BaseMinimapPanel.h"
+#include <vector>
 
 class MinimapPanel : public BaseMinimapPanel {
 public:
 	MinimapPanel(wxWindow* parent);
+	void draw();
 
+private:
+	wxSize _boxSize;
+	wxPoint _translation;
+	wxImage _floorImg;
+
+	struct FieldBox {
+		FieldBox(wxImage img, wxPoint pos = wxPoint(0, 0)) : _img(img), _position(pos) {}
+		wxImage _img;
+		wxPoint _position;
+	};
+
+	std::vector<std::vector<FieldBox>> _fields;
 };
